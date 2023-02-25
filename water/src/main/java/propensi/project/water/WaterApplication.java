52216c4d1a0be09b090da45cf854f33dbf8010fb
ghanii <abdul.ghani02@ui.ac.id>
2023-02-26 00:58:07 +0700

    test deploy

--- water/src/main/java/propensi/project/water/WaterApplication.java
+++ water/src/main/java/propensi/project/water/WaterApplication.java
@@ -2,9 +2,17 @@ package propensi.project.water;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WaterApplication {
+
+	@GetMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WaterApplication.class, args);
